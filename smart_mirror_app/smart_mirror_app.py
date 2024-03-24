18a25fdd675531ae9d4dd01f1b9a4408f6ddadcc
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-03-24 13:11:18 +0000

    Join weather_data into smart_mirror_app.py

--- smart_mirror_app/smart_mirror_app.py
+++ smart_mirror_app/smart_mirror_app.py
@@ -1,9 +1,33 @@
 from tkinter import Tk, Frame, Label
 from datetime import datetime
+import pyowm
 
-# Importing modules
-from modules.weather_data import WeatherData
-from modules.news_data import NewsData
+class WeatherData:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.own = pyowm.OWM(api_key) # authentication connection to the api
+
+    def fetch_weather(self, location):
+        try:
+            # Fetch weather for a specified location
+            observation = self.owm.weather_at_place(location)
+            weather = observation.get_weather()
+            
+            temperature = weather.get_temperature('celsius')['temp']
+            status = weather.get_status()
+
+            # Return weather data as a dictionary
+            return{
+                'location': location,
+                'status': status,
+                'temperature': temperature
+            }
+        except pyowm.exceptions.api_response_error.NotFoundError:
+            print('Location not found')
+            return None
+        except pyowm.exceptions.api_call_error.APICallError as e:
+            print(f"Error fetching weather data: {e}")
+            return None
 
 class SmartMirrorApp:
     def __init__(self, root):
@@ -12,9 +36,11 @@ class SmartMirrorApp:
         self.root.geometry("1920x1080")
         self.root.configure(background='black')
         self.root.attributes('-topmost', 1)
-        self.root.attributes('fullscreen', True) 
-        self.root.iconbitmap("mirror_86251.ico")
+        self.root.attributes('-fullscreen', True)
+        # self.root.iconbitmap('mirror_86251.ico')
 
-        # Initialize weather data and news data objects
-        self.weather_data = WeatherData()
-        self.news_data = NewsData()
+
+if __name__ == "__main__":
+    root = Tk()
+    app = SmartMirrorApp(root)
+    app.root.mainloop()
