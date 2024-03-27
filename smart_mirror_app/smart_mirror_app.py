320a537e7c1917a7e37ffbba8ff4bee37fe041a0
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-03-27 16:40:21 +0000

    Add weather display functionality in smart_mirror_app.py
    - Introduce a WeatherDisplay frame to show weather information on the smart mirror.
    - Implement weather widget with city, description and temperature.
    - It incorporates a method for updating weather data retrieved from
    the WeatherData object, so that there is a real-time display of weather
    information on the Smart Mirror GUI.

--- smart_mirror_app/smart_mirror_app.py
+++ smart_mirror_app/smart_mirror_app.py
@@ -12,9 +12,10 @@ class WeatherData:
         try:
             response = requests.get(self.url) # send an HTTP GET request to the url
             data = response.json() # save data in JSON 
+            return data, response
         except requests.RequestException as e: # all exceptions from the Request library
             print(f'Error fetching weather data: {e}')
-            return None
+            return None, None
 
     def extract_weather_info(self, data, response):  # method to handle response received 
         if response.status_code == 200:   # if request is successfull extract information
@@ -28,6 +29,39 @@ class WeatherData:
            }
         else:
             print("Failed to retrieve weather data")
+            return None
+class WeatherDisplay(Frame):
+    def __init__(self, parent, api_key, city, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.parent = parent
+        self.configure(bg='black')
+        self.weather_data = WeatherData(api_key, city)
+        self.weather_widget()
+        self.update_weather()
+
+    def weather_widget(self):
+        # labels for displaying weather information
+        self.temp_label = Label(self, font=('Courier', 28), text="", fg='White', bg="black")
+        self.temp_label.grid(row=0, column=0, padx=5, pady=3, sticky='ne')
+
+        self.describe_label = Label(self, font=('Courier', 13), text="", fg='White', bg="black")
+        self.describe_label.grid(row=1, column=0, padx=5, pady=3, sticky='ne')
+
+        self.city_label = Label(self, font=('Courier', 13), text="", fg='White', bg="black")
+        self.city_label.grid(row=2, column=0, padx=5, pady=3, sticky='ne')
+    
+    def update_weather(self):
+        # unpacking the tuple retured by fetch_weather
+        data, response = self.weather_data.fetch_weather()
+        weather_info = self.weather_data.extract_weather_info(data, response)
+
+        if weather_info:
+            self.temp_label.config(text=f"{weather_info['temperature']}°C")
+            self.describe_label.config(text=weather_info['description'])
+            self.city_label.config(text=weather_info['city'])
+        else:
+            self.temp_label.config(text='Failed to retrieve weather data')
+
 class SmartMirrorApp:
     def __init__(self, root):
         self.root = root
@@ -35,6 +69,10 @@ class SmartMirrorApp:
         self.root.configure(background='black')
         self.root.attributes('-topmost', 1)
         self.root.attributes('-fullscreen', True)
+
+    # Weather display frame
+        self.weather_display = WeatherDisplay(root, api_key='3e28611a96f98531af8cdf8f67545878', city= 'London' )
+        self.weather_display.place(relx=1, rely=0, anchor='ne')
 
 if __name__ == "__main__":
     root = Tk()
