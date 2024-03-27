67ca3cbeb8b4e3929b29f752af1de258646036d6
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-03-27 11:47:11 +0000

    Refactor weather data fetching implementation in smart_mirror_app.py
    Replace the pyowm library with requests to obtain weather data
    Add fetch_weather function to WeatherData class to retrieve weather data from the OpenWeatherMap API.
    Handle exceptions with requests.RequestException for all exceptions

--- smart_mirror_app/smart_mirror_app.py
+++ smart_mirror_app/smart_mirror_app.py
@@ -1,44 +1,31 @@
 from tkinter import Tk, Frame, Label
-from datetime import datetime
-import pyowm
+import requests
 
+# class to fetch weather data 
 class WeatherData:
-    def __init__(self, api_key):
+    def __init__(self, api_key, city):
         self.api_key = api_key
-        self.own = pyowm.OWM(api_key) # authentication connection to the api
+        self.city = city
+        self.url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
-    def fetch_weather(self, location):
+    def fetch_weather(self): # method to fetch weather data from the API
         try:
-            # Fetch weather for a specified location
-            observation = self.owm.weather_at_place(location)
-            weather = observation.get_weather()
-            
-            temperature = weather.get_temperature('celsius')['temp']
-            status = weather.get_status()
-
-            # Return weather data as a dictionary
-            return{
-                'location': location,
-                'status': status,
-                'temperature': temperature
-            }
-        except pyowm.exceptions.api_response_error.NotFoundError:
-            print('Location not found')
+            response = requests.get(self.url) # send an HTTP GET request to the url
+            data = response.json() # save data in JSON 
+        except requests.RequestException as e: # all exceptions from the Request library
+            print(f'Error fetching weather data: {e}')
             return None
-        except pyowm.exceptions.api_call_error.APICallError as e:
-            print(f"Error fetching weather data: {e}")
-            return None
+    
+    def extract_weather_info(self):
+        pass
 
 class SmartMirrorApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Smart Mirror")
         self.root.geometry("1920x1080")
         self.root.configure(background='black')
         self.root.attributes('-topmost', 1)
         self.root.attributes('-fullscreen', True)
-        # self.root.iconbitmap('mirror_86251.ico')
-
 
 if __name__ == "__main__":
     root = Tk()
